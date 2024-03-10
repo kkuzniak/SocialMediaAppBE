1c5902cb6fb819fe4d07924a7f6d917dfcf61f0d
Kacper Kuzniak <kkuzniakgithub@gmail.com>
2024-03-10 22:54:34 +0100

    Remove user controllers

--- src/User/routes.ts
+++ src/User/routes.ts
@@ -1,14 +1,11 @@
 import { Router } from 'express';
 
 import { login, logout, signup } from '../Auth/controller';
-import { getUsers } from './controller';
 
 const userRouter = Router();
 
 userRouter.post('/signup', signup);
 userRouter.post('/login', login);
 userRouter.get('/logout', logout);
-
-userRouter.route('/').get(getUsers);
 
 export default userRouter;
