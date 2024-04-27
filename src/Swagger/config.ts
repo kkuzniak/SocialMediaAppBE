fc2a8c080ce14c80f2847e47e173a6baf3cc84b4
Kacper Kuzniak <kkuzniakgithub@gmail.com>
2024-04-27 23:11:09 +0200

    22 Add swagger components as a yaml files

--- src/Swagger/config.ts
+++ src/Swagger/config.ts
@@ -8,13 +8,11 @@ const swaggerDefinition = {
     description:
       'SocialMediaApp API documentation. <br><br>_If you want to test out the API please use <b>/v1/users/login</b> endpoint to log in as a test user first or just create your user with the <b>/v1/users/signup</b> endpoint._',
   },
-  host: 'http://localhost:3005',
-  basePath: '/v1',
 };
 
 const options = {
   swaggerDefinition,
-  apis: ['./src/*/routes.ts'],
+  apis: ['./src/*/components.yaml', './src/*/routes.ts'],
 };
 
 export const swaggerSpecification = swaggerJSDoc(options);
